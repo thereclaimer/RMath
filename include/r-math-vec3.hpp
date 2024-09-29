0713cfc61bb39102778f79d14def208a93f00be5
Sam Davenport <sam@davenportdronographics.com>
2024-09-29 10:51:04 -0500

    defining vec3 functions

--- include/r-math-vec3.hpp
+++ include/r-math-vec3.hpp
@@ -13,17 +13,98 @@ typedef RMathVec3 r_v3;
 
 namespace r_math {
 
-    r_external r_void vec3_add      (r_v3* in_v3_a, r_v3* in_v3_b, r_v3*  out_v3_a_plus_b,  const r_size count);
-    r_external r_void vec3_subtract (r_v3* in_v3_a, r_v3* in_v3_b, r_v3*  out_v3_a_sub_b,   const r_size count);
-    r_external r_void vec3_dot      (r_v3* in_v3_a, r_v3* in_v3_b, r_f32* out_a_dot_b,      const r_size count);
-    r_external r_void vec3_cross    (r_v3* in_v3_a, r_v3* in_v3_b, r_v3*  out_v3_a_cross_b, const r_size count);
+    r_void vec3_add             (r_v3* in_v3_a, r_v3* in_v3_b, r_v3*  out_v3_a_plus_b,  const r_size count);
+    r_void vec3_subtract        (r_v3* in_v3_a, r_v3* in_v3_b, r_v3*  out_v3_a_sub_b,   const r_size count);
+    r_void vec3_dot             (r_v3* in_v3_a, r_v3* in_v3_b, r_f32* out_a_dot_b,      const r_size count);
+    r_void vec3_cross           (r_v3* in_v3_a, r_v3* in_v3_b, r_v3*  out_v3_a_cross_b, const r_size count);
+    r_void vec3_multiply_scalar (r_v3* in_v3, r_v3* out_v3, const r_f32 scalar, const r_size count);
+    r_void vec3_divide_scalar   (r_v3* in_v3, r_v3* out_v3, const r_f32 scalar, const r_size count);
+    r_void vec3_magnitude       (r_v3* in_v3, r_f32* out_magnitude,     const r_size count);
+    r_void vec3_normalize       (r_v3* in_v3, r_v3*  out_v3_normalized, const r_size count);
 
-    r_external r_void vec3_multiply_scalar (r_v3* in_v3, r_v3* out_v3, const r_f32 scalar, const r_size count);
-    r_external r_void vec3_divide_scalar   (r_v3* in_v3, r_v3* out_v3, const r_f32 scalar, const r_size count);
-    
-    r_external r_void vec3_magnitude (r_v3* in_v3, r_f32* out_magnitude,     const r_size count);
-    r_external r_void vec3_normalize (r_v3* in_v3, r_v3*  out_v3_normalized, const r_size count);
+    r_void vec3_add_simd_aligned(r_v3* in_v3_a, r_v3* in_v3_b, r_v3*  out_v3_a_plus_b,  const r_size count);
+
 };
 
+inline r_void
+r_math::vec3_add(
+          r_v3*  in_v3_a,
+          r_v3*  in_v3_b,
+          r_v3*  out_v3_a_plus_b,
+    const r_size count) {
+
+}
+
+inline r_void
+r_math::vec3_subtract(
+          r_v3*  in_v3_a,
+          r_v3*  in_v3_b,
+          r_v3*  out_v3_a_sub_b,
+    const r_size count) {
+
+}
+
+inline r_void 
+r_math::vec3_dot(
+          r_v3*  in_v3_a,
+          r_v3*  in_v3_b,
+          r_f32* out_a_dot_b,
+    const r_size count) {
+
+}
+
+inline r_void
+r_math::vec3_cross(
+          r_v3*  in_v3_a,
+          r_v3*  in_v3_b,
+          r_v3*  out_v3_a_cross_b,
+    const r_size count) {
+
+}
+
+inline r_void
+r_math::vec3_multiply_scalar(
+          r_v3*  in_v3,
+          r_v3*  out_v3,
+    const r_f32  scalar,
+    const r_size count) {
+
+}
+
+inline r_void
+r_math::vec3_divide_scalar(
+          r_v3*  in_v3,
+          r_v3*  out_v3,
+    const r_f32  scalar,
+    const r_size count) {
+
+}
+
+inline r_void
+r_math::vec3_magnitude(
+          r_v3*  in_v3,
+          r_f32* out_magnitude,
+    const r_size count) {
+
+}
+
+inline r_void
+r_math::vec3_normalize(
+          r_v3*  in_v3,
+          r_v3*  out_v3_normalized,
+    const r_size count) {
+
+}
+
+inline r_void 
+r_math::vec3_add_simd_aligned(
+          r_v3*  in_v3_a,
+          r_v3*  in_v3_b,
+          r_v3*  out_v3_a_plus_b,
+    const r_size count) {
+
+
+
+}
 
 #endif //R_MATH_VEC3_HPP
--- include/r-math.hpp
+++ include/r-math.hpp
@@ -1,6 +1,6 @@
 #ifndef R_MATH_HPP
 #define R_MATH_HPP
 
-#include "r-math-vec2.hpp"
+
 
 #endif //R_MATH_HPP
